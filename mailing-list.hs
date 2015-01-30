055cece9ac34994862eaa8f9477dce5aab6f4cb1
Daniel Brice <danielbrice@gmail.com>
2015-01-30 11:34:48 -0600

    removed binaries from repository

--- mailing-list.hs
+++ mailing-list.hs
@@ -2,6 +2,8 @@ geographys = [ "Souther California"
              , "California"
              , "West Coast"
              , "Western"
+             , "Southwest"
+             , "South West"
              ]
 
 subjects = [ "Mathematics"
