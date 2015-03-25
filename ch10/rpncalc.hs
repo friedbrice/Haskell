38e423e78362d610e3d1794338f08e1157241d04
Daniel Brice <danielbrice@gmail.com>
2015-03-25 11:37:30 -0500

    finally finished ch 9. rpncalc.hs is an interactive executable now, rather than just a function.

---
+++ ch10/rpncalc.hs
@@ -0,0 +1,37 @@
+solveRPN :: String -> Float
+solveRPN = head . foldl f [] . words where
+	f (x:xs) "id"    = (id x):xs
+	f (x:xs) "acos"  = (acos x):xs
+	f (x:xs) "acosh" = (acosh x):xs
+	f (x:xs) "asin"  = (asin x):xs
+	f (x:xs) "asinh" = (asinh x):xs
+	f (x:xs) "atan"  = (atan x):xs
+	f (x:xs) "atanh" = (atanh x):xs
+	f (x:xs) "cos"   = (cos x):xs
+	f (x:xs) "cosh"  = (cosh x):xs
+	f (x:xs) "exp"   = (exp x):xs
+	f (x:xs) "ln"    = (log x):xs
+	f (x:xs) "sin"   = (sin x):xs
+	f (x:xs) "sinh"  = (sinh x):xs
+	f (x:xs) "sqrt"  = (sqrt x):xs
+	f (x:xs) "tan"   = (tan x):xs
+	f (x:xs) "tanh"  = (tanh x):xs
+	f (x:y:ys) "+"   = (y + x):ys
+	f (x:y:ys) "-"   = (y - x):ys
+	f (x:y:ys) "*"   = (y * x):ys
+	f (x:y:ys) "/"   = (y / x):ys
+	f (x:y:ys) "^"   = (y ** x):ys
+	f (x:y:ys) "log" = (logBase y x):ys
+	f xs "sum"       = [sum xs]
+	f xs "prod"      = [product xs]
+	f xs "avg"       = [(sum xs)/(fromIntegral . length $ xs)]
+	f xs "max"       = [maximum xs]
+	f xs "min"       = [minimum xs]
+	f xs n           = (read n):xs
+
+main :: IO ()
+main = do
+	putStrLn "rpncalc: a console reverse Polish notation calculator."
+	putStrLn "Type an expression in RPN and hit enter to evaluate."
+	putStrLn "Press [ctrl] + [d] to exit."
+	interact $ unlines . map (show . solveRPN) . lines
