d8d207247feb9c5fa1d6d9b32322d791df42225c
Daniel Brice <danielbrice@gmail.com>
2015-05-26 23:42:47 -0700

    new toys! new toys!

--- integrator/integrator.hs
+++ integrator/integrator.hs
@@ -7,10 +7,10 @@ leftRiemannSum :: Function  -- ^ Function to be integrated.
                -> Double    -- ^ Right boundary of integration.
                -> Integer   -- ^ Number of rectangles.
                -> Double    -- ^ Approximate integral.
-leftRiemannSum f a b n = (sum ys)*dx
+leftRiemannSum f a b n = sum ys * dx
   where
-    dx = (b - a) / (fromInteger n)
-    xs = map (\k -> a + (fromInteger k)*dx) [0..(n-1)]
+    dx = (b - a) / fromInteger n
+    xs = map (\k -> a + fromInteger k * dx) [0..(n-1)]
     ys = map f xs
 
 -- | Returns Riemann sum using right endpoints.
@@ -19,8 +19,8 @@ rightRiemannSum :: Function -- ^ Function to be integrated.
                 -> Double   -- ^ Right boundary of integration.
                 -> Integer  -- ^ Number of rectangles.
                 -> Double   -- ^ Approximate integral.
-rightRiemannSum f a b n = (sum ys)*dx
+rightRiemannSum f a b n = sum ys * dx
   where
-    dx = (b - a) / (fromInteger n)
-    xs = map (\k -> a + (fromInteger k)*dx) [1..n]
+    dx = (b - a) / fromInteger n
+    xs = map (\k -> a + fromInteger k * dx) [1..n]
     ys = map f xs
--- quadratic-to-qubezier.hs
+++ quadratic-to-qubezier.hs
@@ -1,16 +1,25 @@
 -- quadratic-to-qubezier.hs
-import System.Environment
+import System.Environment (getArgs)
 
-quadToQbez :: [Float] -> [(Float, Float)]
-quadToQbez (a:b:c:cs) = [(x0, y0), (x1, y1), (x2, y2)] where
-	x0 = -b / (2 * a) - 1
-	y0 = f x0
-	x1 = -b / (2 * a)
-	y1 = f x0 + f' x0
-	x2 = -b / (2 * a) + 1
-	y2 = f x2
-	f  = (\x -> a * x^2 + b * x + c)
-	f' = (\x -> 2 * a * x + b)
+-- | Converts y = Ax^2 + Bx + C to QBezier notation.
+quadToQbez :: Float             -- ^ Coefficient of x^2.
+           -> Float             -- ^ Coefficient of x.
+           -> Float             -- ^ Constant term.
+           -> [(Float, Float)]  -- ^ Curve in QBez notation.
+quadToQbez a b c = [(x0, y0), (x1, y1), (x2, y2)]
+  where
+	x0   = -b / (2 * a) - 1
+	y0   = f x0
+	x1   = -b / (2 * a)
+	y1   = f x0 + f' x0
+	x2   = -b / (2 * a) + 1
+	y2   = f x2
+	f  x = a * x^2 + b * x + c
+	f' x = 2 * a * x + b
 
+-- | Console wrapper for quadToQbez.
 main :: IO ()
-main = print . quadToQbez . map read =<< getArgs
+main = print . unQuadToQbez . toTuple . map read =<< getArgs
+  where
+    unQuadToQbez = uncurry . uncurry $ quadToQbez
+    toTuple (a:b:c:[]) = ((a, b), c)
