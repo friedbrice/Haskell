ccf3461db71cc8809f838140f75112799fbf1ec7
Daniel Brice <danielbrice@gmail.com>
2015-06-24 00:16:10 -0700

    made some minor stylistic changes to algorithm-x.hs

--- algorithm-x/algorithm-x.hs
+++ algorithm-x/algorithm-x.hs
@@ -6,9 +6,8 @@ type Vector = [Int]
 type Matrix = [Vector]
 type LabelMatrix = [(Int, Vector)]
 
-sumRows :: Matrix -> Vector
-sumRows [] = repeat 0
-sumRows (x:xs) = zipWith (+) x (sumRows xs)
+fromString :: String -> Matrix
+fromString = map (map read . words) . lines
 
 label :: Matrix -> LabelMatrix
 label = zip [1..]
@@ -16,15 +15,15 @@ label = zip [1..]
 unlabel :: LabelMatrix -> Matrix
 unlabel = map snd
 
+sumRows :: Matrix -> Vector
+sumRows = foldr (zipWith (+)) (repeat 0)
+
 exactCovers :: Matrix -> [LabelMatrix]
 exactCovers m = do
     let ones = replicate (length . head $ m) 1
     s <- subsequences . label $ m
     guard $ (sumRows . unlabel $ s) == ones
     return s
-
-fromString :: String -> Matrix
-fromString = map (map read) . map words . lines
 
 main :: IO ()
 main = print . exactCovers . fromString =<< getContents
